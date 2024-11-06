a4b8ea17ba626ec90ad49d9899e036d23f4a3003
Nathan Leiby <nathanleiby@gmail.com>
2024-11-06 11:01:07 -0800

    2024-11-06: predict hand from video

--- 2024-11-06/sketch.js
+++ 2024-11-06/sketch.js
@@ -1,15 +1,20 @@
 let handpose;
 let predictions = [];
 let img;
+let video;
 
 // happens before the main program starts
 function preload() {
   img = loadImage("data/hand.jpg");
 }
 function setup() {
-  createCanvas(400, 350);
-  handpose = ml5.handpose(modelReady);
-  frameRate(1); // don't need fast frame rate
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  handpose = ml5.handpose(video, modelReady);
+  // frameRate(5); // don't need fast frame rate
 }
 
 function modelReady() {
@@ -17,39 +22,29 @@ function modelReady() {
   handpose.on("predict", (results) => {
     predictions = results;
   });
-  handpose.predict(img);
+  handpose.predict(video);
 }
 
 function draw() {
+  // background(20);
   // display the image
-  image(img, 0, 0, width, height);
+  // tint(255, 16);
+  image(video, 0, 0, width, height);
+  tint(255, 255);
 
   drawKeypoints();
-
-  // don't call draw() again
-  noLoop();
 }
 
 function drawKeypoints() {
-  if (predictions.length > 0) {
-    /* array for each prediction point
-              indicies 0 - 4 : thumb
-              indicies 5 - 8 : pointer
-              indicies 9 - 12 : middle finger
-              indicies 13 - 16 : ring finger
-              indicies 17 - 20 : pinky
-           */
-
-    let p = predictions[0];
-    let keypoints = p.landmarks;
-
-    console.log(p);
-
-    // draw an ellipse at each keypoint
-    for (let kp of keypoints) {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const prediction = predictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
       fill(0, 255, 0);
       noStroke();
-      ellipse(kp[0], kp[1], 10, 10);
+      // tint(255, 128);
+      ellipse(keypoint[0], keypoint[1], 10, 10);
+      tint(255, 255);
     }
   }
 }
